1f25add126d8d3d0cbf6a03b4f4ad2e11c39c834
Caleb Shaw <shawcaleb1688@gmail.com>
2025-03-11 00:43:25 -0400

    workin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
-import Overview from "./components/Overview";
-import SignUp from "./components/Signup";
-import Footer from "./components/Footer";
-import DownloadRulebook from "./components/Rulebook";
-import HeroSection from "./components/HeroSection";
+
 import './styles/styles.css'
 import './styles/fonts.css';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/react';
+
+
 import UnderConstruction from "./components/UnderConstruction";
 
 function App() {
